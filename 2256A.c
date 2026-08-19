d7d2629cd7674e4f5bdc2fc206eb1db9517041fd
tensor-08 <tanish208k@gmail.com>
2026-08-19 10:32:56 +0530

    Fixed

--- 2256A.c
+++ 2256A.c
@@ -19,6 +19,66 @@ int main(void)
             printf("0\n");
         }
 
+        else if (numbers[i][0] == numbers[i][1])
+        {
+            if (numbers[i][0] > numbers[i][2])
+            {
+                printf("%d\n", numbers[i][0] - numbers[i][2]);
+            }
+            else
+            {
+                if(2 * numbers[i][0] >= numbers[i][2])
+                {
+                    printf("%d", numbers[i][2] - numbers[i][0]);
+                }
+                if(2 * numbers[i][0] < numbers[i][2])
+                {
+                    numbers[i][2] = 2 * numbers[i][0];
+                    printf("%d", numbers[i][2] - numbers[i][0]);
+                }
+            }
+        }
+
+        else if (numbers[i][1] == numbers[i][2])
+        {
+            if (numbers[i][1] > numbers[i][0])
+            {
+                printf("%d\n", numbers[i][1] - numbers[i][0]);
+            }
+            else
+            {
+                if(2 * numbers[i][1] >= numbers[i][0])
+                {
+                    printf("%d", numbers[i][0] - numbers[i][1]);
+                }
+                if(2 * numbers[i][1] < numbers[i][0])
+                {
+                    numbers[i][0] = 2 * numbers[i][1];
+                    printf("%d", numbers[i][0] - numbers[i][1]);
+                }
+            }
+        }
+
+        else if (numbers[i][0] == numbers[i][2])
+        {
+            if (numbers[i][0] > numbers[i][1])
+            {
+                printf("%d\n", numbers[i][0] - numbers[i][1]);
+            }
+            else
+            {
+                if(2 * numbers[i][0] >= numbers[i][1])
+                {
+                    printf("%d", numbers[i][1] - numbers[i][0]);
+                }
+                if(2 * numbers[i][0] < numbers[i][1])
+                {
+                    numbers[i][1] = 2 * numbers[i][0];
+                    printf("%d", numbers[i][1] - numbers[i][0]);
+                }
+            }
+        }
+
         else if (numbers[i][0] + numbers[i][1] > numbers[i][2] && numbers[i][2] > numbers[i][0] && numbers[i][2] > numbers[i][1])
         {
             if(numbers[i][0] > numbers[i][1])
