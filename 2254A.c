c3e64cb88b72c0a8b210d4d90b50474af1a4c096
tensor-08 <tanish208k@gmail.com>
2026-08-21 17:51:28 +0000

    2254A accepted

--- 2254A.c
+++ 2254A.c
@@ -8,29 +8,46 @@ int main(void)
     scanf("%d", &n);
 
     int numbers[n][3];
+    int j;
 
     for (int i = 0; i < n; i++)
     {
         scanf("%d %d %d", &numbers[i][0], &numbers[i][1], &numbers[i][2]);
-        sort3(&numbers[i][0], &numbers[i][1], &numbers[i][2]);
     }
 
     for(int i = 0; i < n; i++)
     {
-        if(numbers[i][2] >= numbers[i][0] + numbers[i][1])
+        j = 0;
+        do
         {
-            printf("%d\n", numbers[i][1]);
-        }
-        else
-        {
-            printf("%d\n", numbers[i][2] - numbers[i][0]);
-        }
+            if (numbers[i][0] == numbers[i][1] || numbers[i][1] == numbers[i][2] || numbers[i][2] == numbers[i][0])
+            {
+                break;
+            }
+            else
+            {
+                sort3(&numbers[i][0], &numbers[i][1], &numbers[i][2]);
+                numbers[i][2] = numbers[i][2] - 1;
+                numbers[i][0] = numbers[i][0] + 1;
+                j++;
+            }
+        } while (0 == 0);
+        printf("%d\n", j);
     }
 }
 
 void sort3(int *a, int *b, int *c)
 {
-    if (*a > *b) { int temp = *a; *a = *b; *b = temp; }
-    if (*b > *c) { int temp = *b; *b = *c; *c = temp; }
-    if (*a > *b) { int temp = *a; *a = *b; *b = temp; }
+    if (*a > *b)
+    {
+        int temp = *a; *a = *b; *b = temp;
+    }
+    if (*b > *c)
+    {
+        int temp = *b; *b = *c; *c = temp;
+    }
+    if (*a > *b) 
+    {
+        int temp = *a; *a = *b; *b = temp;
+    }
 }
