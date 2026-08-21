aa5623875e9575932b38fdd9d80e1c6cc0c58268
tensor-08 <tanish208k@gmail.com>
2026-08-21 16:48:12 +0000

    using sorts

--- 2256A.c
+++ 2256A.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+void sort3(int *a, int *b, int *c);
+
 int main(void)
 {
     int n;
@@ -10,144 +12,14 @@ int main(void)
     for (int i = 0; i < n; i++)
     {
         scanf("%d %d %d", &numbers[i][0], &numbers[i][1], &numbers[i][2]);
-    }
+        sort3(&numbers[i][0], &numbers[i][1], &numbers[i][2]);
+        printf("%d  %d  %d", numbers[i][0], numbers[i][1], numbers[i][2]);
+    }    
+}
 
-    for (int i =0; i < n; i++)
-    {
-        if(numbers[i][0] == numbers[i][1] && numbers[i][1] == numbers[i][2])
-        {
-            printf("0\n");
-        }
-
-        else if (numbers[i][0] == numbers[i][1])
-        {
-            if (numbers[i][0] > numbers[i][2])
-            {
-                printf("%d\n", numbers[i][0] - numbers[i][2]);
-            }
-            else
-            {
-                if(2 * numbers[i][0] >= numbers[i][2])
-                {
-                    printf("%d\n", numbers[i][2] - numbers[i][0]);
-                }
-                if(2 * numbers[i][0] < numbers[i][2])
-                {
-                    numbers[i][2] = 2 * numbers[i][0];
-                    printf("%d\n", numbers[i][2] - numbers[i][0]);
-                }
-            }
-        }
-
-        else if (numbers[i][1] == numbers[i][2])
-        {
-            if (numbers[i][1] > numbers[i][0])
-            {
-                printf("%d\n", numbers[i][1] - numbers[i][0]);
-            }
-            else
-            {
-                if(2 * numbers[i][1] >= numbers[i][0])
-                {
-                    printf("%d\n", numbers[i][0] - numbers[i][1]);
-                }
-                if(2 * numbers[i][1] < numbers[i][0])
-                {
-                    numbers[i][0] = 2 * numbers[i][1];
-                    printf("%d\n", numbers[i][0] - numbers[i][1]);
-                }
-            }
-        }
-
-        else if (numbers[i][0] == numbers[i][2])
-        {
-            if (numbers[i][0] > numbers[i][1])
-            {
-                printf("%d\n", numbers[i][0] - numbers[i][1]);
-            }
-            else
-            {
-                if(2 * numbers[i][0] >= numbers[i][1])
-                {
-                    printf("%d\n", numbers[i][1] - numbers[i][0]);
-                }
-                if(2 * numbers[i][0] < numbers[i][1])
-                {
-                    numbers[i][1] = 2 * numbers[i][0];
-                    printf("%d\n", numbers[i][1] - numbers[i][0]);
-                }
-            }
-        }
-
-        else if (numbers[i][0] + numbers[i][1] > numbers[i][2] && numbers[i][2] > numbers[i][0] && numbers[i][2] > numbers[i][1])
-        {
-            if(numbers[i][0] > numbers[i][1])
-            {
-                printf("%d\n", numbers[i][2] - numbers[i][1]);
-            }
-            else
-            {
-                printf("%d\n", numbers[i][2] - numbers[i][0]);
-            }
-        }
-        else if (numbers[i][0] + numbers[i][2] > numbers[i][1] && numbers[i][1] > numbers[i][0] && numbers[i][1] > numbers[i][2])
-        {
-            if(numbers[i][0] > numbers[i][1])
-            {
-                printf("%d\n", numbers[i][1] - numbers[i][2]);
-            }
-            else
-            {
-                printf("%d\n", numbers[i][1] - numbers[i][0]);
-            }
-        }
-        else if (numbers[i][2] + numbers[i][1] > numbers[i][0] && numbers[i][0] > numbers[i][2] && numbers[i][0] > numbers[i][1])
-        {
-            if(numbers[i][0] > numbers[i][1])
-            {
-                printf("%d\n", numbers[i][0] - numbers[i][1]);
-            }
-            else
-            {
-                printf("%d\n", numbers[i][0] - numbers[i][2]);
-            }
-        }
-        else if(numbers[i][0] > numbers[i][1] && numbers[i][0] > numbers[i][2])
-        {
-            numbers[i][0] = numbers[i][1] + numbers[i][2];
-            if(numbers[i][1] < numbers[i][2])
-            {
-                printf("%d\n", numbers[i][0] - numbers[i][1]);
-            }
-            else
-            {
-                printf("%d\n", numbers[i][0] - numbers[i][2]);
-            }
-        }
-        else if(numbers[i][1] > numbers[i][0] && numbers[i][1] > numbers[i][2])
-        {
-            numbers[i][1] = numbers[i][0] + numbers[i][2];
-            if(numbers[i][0] < numbers[i][2])
-            {
-                printf("%d\n", numbers[i][1] - numbers[i][0]);
-            }
-            else
-            {
-                printf("%d\n", numbers[i][1] - numbers[i][2]);
-            }
-        }
-        else if(numbers[i][2] > numbers[i][1] && numbers[i][2] > numbers[i][0])
-        {
-            numbers[i][2] = numbers[i][1] + numbers[i][0];
-            if(numbers[i][1] < numbers[i][0])
-            {
-                printf("%d\n", numbers[i][2] - numbers[i][1]);
-            }
-            else
-            {
-                printf("%d\n", numbers[i][2] - numbers[i][0]);
-            }
-        }
-
-    }
+void sort3(int *a, int *b, int *c)
+{
+    if (*a > *b) { int temp = *a; *a = *b; *b = temp; }
+    if (*b > *c) { int temp = *b; *b = *c; *c = temp; }
+    if (*a > *b) { int temp = *a; *a = *b; *b = temp; }
 }
